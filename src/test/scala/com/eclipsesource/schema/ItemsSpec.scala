b012a78c0fe3f8c65de5ed153b0d0807d50299df
Edgar Mueller <emueller@eclipsesource.com>
2016-03-31 14:40:36 +0200

    Added additional test case for covering invalid items within array

--- src/test/scala/com/eclipsesource/schema/ItemsSpec.scala
+++ src/test/scala/com/eclipsesource/schema/ItemsSpec.scala
@@ -2,8 +2,28 @@ package com.eclipsesource.schema
 
 import com.eclipsesource.schema.test.JsonSpec
 import org.specs2.mutable.Specification
+import play.api.libs.json.Json
 
 class ItemsSpec extends Specification with JsonSpec {
   validate("items")
+
+
+  "validate array with some invalid items" in {
+    val s = JsonSource.schemaFromString(
+      """{
+        |"type": "array",
+        |"items": {
+        | "minimum": 3
+        |}
+        |}""".stripMargin).get
+
+    val i = Json.arr(2, 3, 4, 1)
+
+    val result = SchemaValidator.validate(s)(i)
+    result.isFailure must beTrue
+    result.asEither must beLeft.like { case error =>
+      error.toJson.value must haveLength(2)
+    }
+  }
 }
 
