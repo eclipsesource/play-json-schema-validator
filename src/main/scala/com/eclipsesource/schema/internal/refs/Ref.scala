12b896fb8c348840961abd107b99756f531e7122
Edgar Mueller <emueller@eclipsesource.com>
2016-07-17 14:12:40 +0200

    WIP ajv test #13

---
+++ src/main/scala/com/eclipsesource/schema/internal/refs/Ref.scala
@@ -0,0 +1,8 @@
+package com.eclipsesource.schema.internal.refs
+
+/**
+  * Created by edgar on 14.07.16.
+  */
+trait Ref {
+
+}
---
+++ src/test/scala/com/eclipsesource/schema/AjvSpecs.scala
@@ -0,0 +1,8 @@
+package com.eclipsesource.schema
+
+/**
+  * Created by edgar on 14.07.16.
+  */
+class AjvSpecs {
+
+}
