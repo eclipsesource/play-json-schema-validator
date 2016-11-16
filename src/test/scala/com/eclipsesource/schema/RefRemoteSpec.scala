edb06b4b70518169b1b63433f05f6d9aa84462ad
Edgar Mueller <emueller@eclipsesource.com>
2016-11-16 18:03:03 +0100

    Add specs that explicitly test for error reports

--- src/test/scala/com/eclipsesource/schema/RefRemoteSpec.scala
+++ src/test/scala/com/eclipsesource/schema/RefRemoteSpec.scala
@@ -1,8 +1,94 @@
 package com.eclipsesource.schema
 
-/**
-  * Created by edgar on 14.11.16.
-  */
-class RefRemoteSpec {
+import com.eclipsesource.schema.test.JsonSpec
+import controllers.Assets
+import org.specs2.mutable.Specification
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.libs.json._
+import play.api.mvc.Handler
+import play.api.test.WithServer
 
+class RefRemoteSpec extends Specification with JsonSpec {
+
+  val routes: PartialFunction[(String, String), Handler] = {
+    case (_, path) => Assets.versioned("/", path)
+  }
+
+  def createApp = new GuiceApplicationBuilder().routes(routes).build()
+
+  "remote ref - remote ref invalid" in new WithServer(createApp, port = 1234) {
+    val validator = SchemaValidator()
+    val schema = JsonSource.schemaFromString(
+      """ { "$ref": "http://localhost:1234/remotes/integer.json" } """.stripMargin
+    ).get
+    val instance   = JsString("a")
+    val result     = validator.validate(schema, instance)
+    val errors     = result.asEither.left.get
+    val firstError = errors.toJson(0)
+    (firstError \ "keyword").get.as[String] must beEqualTo("type")
+    result.isError must beTrue
+  }
+
+  "fragment within remote ref - remote fragment invalid" in new WithServer(createApp, port = 1234)  {
+    val schema = JsonSource.schemaFromString(
+      """{"$ref": "http://localhost:1234/remotes/subSchemas.json#/integer"}"""
+    ).get
+    val instance   = JsString("a")
+    val result     = validator.validate(schema, instance)
+    val errors     = result.asEither.left.get
+    val firstError = errors.toJson(0)
+    (firstError \ "schemaPath").get.as[String] must beEqualTo("#/integer")
+    result.isError must beTrue
+  }
+
+  "ref within remote ref - ref within ref invalid" in new WithServer(createApp, port = 1234) {
+    val schema = JsonSource.schemaFromString(
+      """{ "$ref": "http://localhost:1234/remotes/subSchemas.json#/refToInteger" }"""
+    ).get
+    val instance   = JsString("a")
+    val result     = validator.validate(schema, instance)
+    val errors     = result.asEither.left.get
+    val firstError = errors.toJson(0)
+    (firstError \ "schemaPath").get.as[String] must beEqualTo("#/integer")
+    result.isError must beTrue
+  }
+
+  "change resolution scope - change scope ref invalid" in new WithServer(createApp, port = 1234) {
+    val schema = JsonSource.schemaFromString(
+      """{
+        |  "id": "http://localhost:1234/",
+        |  "items": {
+        |    "id": "folder/",
+        |    "items": {"$ref": "folderInteger.json"}
+        |  }
+        |}""".stripMargin
+    ).get
+    val instance   = Json.arr(Json.arr("a"))
+    val result     = validator.validate(schema, instance)
+    val errors     = result.asEither.left.get
+    val firstError = errors.toJson(0)
+    (firstError \ "resolutionScope").get.as[String] must beEqualTo("http://localhost:1234/folder/")
+    (firstError \ "msgs").get.as[JsArray].value.head.as[String] must beEqualTo("Could not resolve ref folderInteger.json")
+    result.isError must beTrue
+  }
+
+  "change resolution scope - change scope ref invalid" in new WithServer(createApp, port = 1234) {
+    val schema = JsonSource.schemaFromString(
+      """{
+        |  "id": "http://localhost:1234/remotes/",
+        |  "items": {
+        |    "id": "folder/",
+        |    "items": {"$ref": "folderInteger.json"}
+        |  }
+        |}""".stripMargin
+    ).get
+    val instance   = Json.arr(Json.arr("a"))
+    val result     = validator.validate(schema, instance)
+    val errors     = result.asEither.left.get
+    val firstError = errors.toJson(0)
+    (firstError \ "schemaPath").get.as[String] must beEqualTo("#")
+    (firstError \ "instancePath").get.as[String] must beEqualTo("/0/0")
+    (firstError \ "resolutionScope").get.as[String] must beEqualTo("http://localhost:1234/remotes/folder/")
+    result.isError must beTrue
+  }
 }
--- src/test/scala/com/eclipsesource/schema/ReferenceSpec.scala
+++ src/test/scala/com/eclipsesource/schema/ReferenceSpec.scala
@@ -2,7 +2,150 @@ package com.eclipsesource.schema
 
 import com.eclipsesource.schema.test.JsonSpec
 import org.specs2.mutable.Specification
+import play.api.libs.json.{JsObject, JsResult, JsString, Json}
 
 class ReferenceSpec extends Specification with JsonSpec {
   validate("ref")
+
+  "root pointer ref - mismatch" in {
+    val validator = SchemaValidator()
+    val recursiveSchema = JsonSource.schemaFromString(
+      """{
+        |  "properties": {
+        |    "foo": {"$ref": "#"}
+        |  },
+        |  "additionalProperties": false
+        |}""".stripMargin)
+
+    val instance = Json.obj("bar" -> false)
+
+    val result = validator.validate(recursiveSchema.get, instance)
+    result.asEither must beLeft.like {
+      case error => (error.toJson(0) \ "instancePath").get.as[String] == ""
+    }
+    result.isError must beTrue
+  }
+
+  "root pointer ref - recursive mismatch" in {
+    val validator = SchemaValidator()
+    val recursiveSchema = JsonSource.schemaFromString(
+      """{
+        |  "properties": {
+        |    "foo": {"$ref": "#"}
+        |  },
+        |  "additionalProperties": false
+        |}""".stripMargin)
+
+    val instance = Json.obj("foo" -> Json.obj("bar" -> false))
+    val result   = validator.validate(recursiveSchema.get, instance)
+    result.asEither must beLeft.like {
+      case error => (error.toJson(0) \ "instancePath").get.as[String] == "/foo"
+    }
+    result.isError must beTrue
+  }
+
+  "relative pointer ref to object - mismatch" in {
+    val schema = JsonSource.schemaFromString(
+      """
+        |{
+        |  "properties": {
+        |    "foo": {"type": "integer"},
+        |    "bar": {"$ref": "#/properties/foo"}
+        |  }
+        |}
+      """.stripMargin)
+    val instance = Json.obj("bar" -> true)
+    val result   = validator.validate(schema.get, instance)
+    val errors   = result.asEither.left.get
+    (errors.toJson(0) \ "instancePath").get.as[String] must beEqualTo("/bar")
+    (errors.toJson(0) \ "schemaPath").get.as[String]   must beEqualTo("#/properties/foo")
+    result.isError must beTrue
+  }
+
+  "relative pointer ref to array - mismatch array" in {
+    val schema = JsonSource.schemaFromString(
+      """
+        |{
+        |  "items": [
+        |    {"type": "integer"},
+        |    {"$ref": "#/items/0"}
+        |  ]
+        |}
+      """.stripMargin)
+    val instance = Json.arr(1, "foo")
+    val result   = validator.validate(schema.get, instance)
+    val errors   = result.asEither.left.get
+
+    (errors.toJson(0) \ "instancePath").get.as[String] must beEqualTo("/1")
+    (errors.toJson(0) \ "schemaPath").get.as[String]   must beEqualTo("#/items/0")
+    (errors.toJson(0) \ "value").get.as[String]        must beEqualTo("foo")
+    result.isError must beTrue
+  }
+
+  "nested refs - nested ref invalid" in {
+    val schema = JsonSource.schemaFromString(
+      """
+        |{
+        |  "definitions": {
+        |    "a": {"type": "integer"},
+        |    "b": {"$ref": "#/definitions/a"},
+        |    "c": {"$ref": "#/definitions/b"}
+        |  },
+        |  "$ref": "#/definitions/c"
+        |}
+      """.stripMargin)
+    val instance = JsString("a")
+    val result   = validator.validate(schema.get, instance)
+    val errors   = result.asEither.left.get
+    (errors.toJson(0) \ "instancePath").get.as[String] must beEqualTo("")
+    (errors.toJson(0) \ "schemaPath").get.as[String]   must beEqualTo("#/definitions/a")
+    (errors.toJson(0) \ "value").get.as[String]        must beEqualTo("a")
+    result.isError must beTrue
+  }
+
+  "remote ref, containing refs itself - remote ref invalid" in {
+    val schema = JsonSource.schemaFromString(
+      """{"$ref": "http://json-schema.org/draft-04/schema#"}""".stripMargin
+    )
+    val instance   = Json.obj("minLength" -> -1)
+    val result     = validator.validate(schema.get, instance)
+    val errors     = result.asEither.left.get
+    val firstError = errors.toJson(0)
+    (firstError \ "instancePath").get.as[String] must beEqualTo("/minLength")
+    (firstError \ "schemaPath").get.as[String]   must beEqualTo("#/definitions/positiveIntegerDefault0")
+    (firstError \ "errors").get must beEqualTo(
+      Json.obj(
+        "/allOf/0" -> Json.arr(
+          Json.obj(
+            "schemaPath" -> "#/allOf/0/definitions/positiveInteger",
+            "errors" -> Json.obj(),
+            "keyword" -> "minimum",
+            "msgs"  -> Json.arr( "minimum violated: -1 is less than 0"),
+            "value" -> -1,
+            "instancePath" -> "/minLength"
+          )
+        )
+      )
+    )
+    (firstError \ "msgs").get must beEqualTo(Json.arr("Instance does not match all schemas"))
+    result.isError must beTrue
+  }
+
+  "property named $ref that is not a reference - property named $ref invalid" in {
+    val schema = JsonSource.schemaFromString(
+      """
+        |{
+        |  "properties": {
+        |    "$ref": {"type": "string"}
+        |  }
+        |}
+      """.stripMargin)
+    val instance   = Json.obj("$ref" -> 2)
+    val result     = validator.validate(schema.get, instance)
+    val errors     = result.asEither.left.get
+    val firstError = errors.toJson(0)
+    (firstError \ "schemaPath").get.as[String] must beEqualTo("#/properties/$ref")
+    (firstError \ "instancePath").get.as[String] must beEqualTo("/$ref")
+    result.isError must beTrue
+  }
 }
