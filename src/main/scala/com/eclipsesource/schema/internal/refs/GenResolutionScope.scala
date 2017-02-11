586efb1d79282eecc1ad7707093455c925c93504
Edgar Mueller <emueller@eclipsesource.com>
2017-02-11 21:39:12 +0100

    Fix for issue #99: relative anchor refs cause infinite recursion

--- src/main/scala/com/eclipsesource/schema/internal/refs/GenResolutionScope.scala
+++ src/main/scala/com/eclipsesource/schema/internal/refs/GenResolutionScope.scala
@@ -5,21 +5,20 @@ import play.api.libs.json.JsPath
 trait GenResolutionContext[A] {
   def refResolver: GenRefResolver[A]
   def scope: GenResolutionScope[A]
-  def schemaPath = scope.schemaPath
-  def instancePath = scope.instancePath
-  val hasBeenVisited = scope.visited.contains _
+  def schemaPath: JsPath = scope.schemaPath
+  def instancePath: JsPath = scope.instancePath
+  val hasBeenVisited: (Ref) => Boolean = scope.visited.contains _
 }
 
-case class GenResolutionScope[A : CanHaveRef](
-                                               documentRoot: A,
-                                               id: Option[Ref] = None, // current resolution scope
-                                               schemaPath: JsPath = JsPath \ "#",
-                                               instancePath: JsPath = JsPath,
-                                               visited: Set[Ref] = Set.empty[Ref] // tracks all visited refs,
-                                             ) {
+case class GenResolutionScope[A : CanHaveRef](documentRoot: A,
+                                              id: Option[Ref] = None, // current resolution scope
+                                              schemaPath: JsPath = JsPath \ "#",
+                                              instancePath: JsPath = JsPath,
+                                              visited: Set[Ref] = Set.empty[Ref], // tracks all visited refs
+                                              depth: Int = 0) {
 
-  def hasBeenVisited = visited.contains _
-  def addVisited(ref: Ref) = copy(visited = visited + ref)
+  def hasBeenVisited: (Ref) => Boolean = visited.contains
+  def addVisited(ref: Ref): GenResolutionScope[A] = copy(visited = visited + ref)
 }
 
 case class DocumentCache[A](private[schema] val mapping: Map[String, A] = Map.empty[String, A]) {
@@ -28,7 +27,7 @@ case class DocumentCache[A](private[schema] val mapping: Map[String, A] = Map.em
 
   def get(id: Ref): Option[A] = mapping.get(id.value)
 
-  def apply(id: Ref) = mapping(id.value)
+  def apply(id: Ref): A = mapping(id.value)
 
   def contains(id: Ref): Boolean = mapping.contains(id.value)
 }
