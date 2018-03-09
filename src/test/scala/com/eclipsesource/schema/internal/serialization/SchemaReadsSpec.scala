0405553b28119c131ce2950c6cce5552bae5a19c
Edgar Mueller <emueller@eclipsesource.com>
2018-03-09 21:22:02 +0100

    Add simple test for strict reads

--- src/test/scala/com/eclipsesource/schema/internal/serialization/SchemaReadsSpec.scala
+++ src/test/scala/com/eclipsesource/schema/internal/serialization/SchemaReadsSpec.scala
@@ -1,9 +1,10 @@
 package com.eclipsesource.schema.internal.serialization
 
+import com.eclipsesource.schema._
 import com.eclipsesource.schema.{JsonSource, SchemaType, SchemaValue}
 import com.eclipsesource.schema.drafts.{Version4, Version7}
 import org.specs2.mutable.Specification
-import play.api.libs.json.{JsBoolean, Json}
+import play.api.libs.json.{JsArray, JsBoolean, JsString, Json}
 
 class SchemaReadsSpec extends Specification {
 
@@ -23,6 +24,15 @@ class SchemaReadsSpec extends Specification {
 
     "not be able to read boolean schema" in {
       Json.fromJson[SchemaType](JsBoolean(true)).isError must beTrue
+    }
+
+    "fail if exclusiveMinimum is not a boolean" in {
+      val result = JsonSource.schemaFromString(
+        """{ "exclusiveMinimum": 3 }""".stripMargin
+      )
+      result.asEither must beLeft.like { case error => (error.toJson(0) \ "msgs").get.as[JsArray].value.head ==
+        JsString("error.expected.jsboolean")
+      }
     }
   }
 
